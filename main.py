183d101951fe38ce8a91bfa9322c5c8ff5ec69fa
sahil <sahilbhetwal22@gmail.com>
2024-02-21 16:48:22 +0545

    done

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ hosts_test = "hosts"
 redirect_ip = "127.0.0.1"
 blocking_website_list = ["www.facebook.com", "facebook. com", "instagram.com","www.instagram.com"]
 while True:
-    while True:
-        if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day, 18):
-            print("Working hours...")
-            with open(hosts_path,"r+") as file:
-                content = file.read()
-                print(content)
-                for website in blocking_website_list: 
-                     if website in content:
-                        pass 
-                     else:
-                        file.write(redirect_ip+" "+ website+"\n")
-        else:
-            with open(hosts_path, 'r+') as file:
-                content = file.readlines()
-                file.seek(0)
-                for line in content:
-                    if not any(website in line for website in blocking_website_list):
-                        file.write(line)
-                file.truncate()
-            print("Fun hours...")
-        time.sleep(5)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month,dt.now().day, 16):
+        print("Working hours...")
+        with open(hosts_path,"r+") as file:
+            content = file.read()
+            print(content)
+            for website in blocking_website_list: 
+                 if website in content:
+                    pass 
+                 else:
+                    file.write(redirect_ip+" "+ website+"\n")
+    else:
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in blocking_website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...")
+    time.sleep(5)
